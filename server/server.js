f545b6e90cdf0ec2eb777e1574bab96e2ba57233
Tryamb Sachan <86840180+Tryamb@users.noreply.github.com>
2023-10-20 23:54:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(cors(
     credentials: true
   }
 ));
+app.options('*', cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/private', require('./routes/private'));
