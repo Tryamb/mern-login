b81f45fde61db237a34cc4cc6e25dd96c52867d1
Tryamb Sachan <86840180+Tryamb@users.noreply.github.com>
2023-10-20 23:18:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors(
-  origin:["https://mern-login-iota.vercel.app"],
+  origin:["https://mern-login-inci.vercel.app"],
   methods:["POST","GET"],
   credentials:true
 ));
