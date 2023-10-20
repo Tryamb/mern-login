a5dad86020407aa13e5a1e95e6d616f5391e79b0
Tryamb Sachan <86840180+Tryamb@users.noreply.github.com>
2023-10-21 00:43:10 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect("mongodb+srv://admin:TiQt8NHT8Qf3w1Rt@cluster0.0tvmiff.mongodb.net/", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
