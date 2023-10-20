81a07ad07e3c157d99760e1062702f98feba018f
Tryamb Sachan <86840180+Tryamb@users.noreply.github.com>
2023-10-21 01:17:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-require('dotenv').config({ path: './config.env' });
+//require('dotenv').config({ path: './config.env' });
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
-connectDB();
+//connectDB();
 const errorHandler = require('./middleware/error');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors(
   {
@@ -16,11 +16,11 @@ app.use(cors(
     credentials: true
   }
 ));
-app.options('*', cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.options('*', cors());
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello');
