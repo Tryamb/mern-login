494d09825858ef9de1640830741bc69f854eac6a
Tryamb Sachan <86840180+Tryamb@users.noreply.github.com>
2023-10-20 23:05:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const errorHandler = require('./middleware/error');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  origin:["https://mern-login-inci.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+));
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/private', require('./routes/private'));
