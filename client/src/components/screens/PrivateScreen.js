f29d9f1b440fb0848afae88402c4d9447d7c609f
Tryamb Sachan <86840180+Tryamb@users.noreply.github.com>
2023-10-21 15:21:43 +0530

    Update PrivateScreen.js

--- client/src/components/screens/PrivateScreen.js
+++ client/src/components/screens/PrivateScreen.js
@@ -41,7 +41,6 @@ const PrivateScreen = ({ history }) => {
     <>
       <div style={{ background: 'black', color: 'white' }}>{privateData}</div>
       <button onClick={logoutHandler}>Logout</button>
-      // <iframe src="#" style={{ width: '100%', height: '500px' }}></iframe>
     </>
   );
 };
