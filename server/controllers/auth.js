f1c3397dbabe1cbe2e6a1f7d4ca924d26eda652d
Tryamb Sachan <86840180+Tryamb@users.noreply.github.com>
2023-10-22 13:25:06 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -72,12 +72,12 @@ exports.forgotPassword = async (req, res, next) => {
     const resetUrl = `mern-login-iota.vercel.app/passwordreset/${resetToken}`;
     const message = `<h1>You have requested a password reset</h1><p>Please go to this link to reset your password</p><a href=${resetUrl} clicktracking=off>${resetUrl}</a>`;
     try {
-      await sendEmail({
+      const result=await sendEmail({
         to: email,
         subject: 'Password Reset Request',
         text: message,
       });
-      res.status(200).json({ success: true, data: `${email}` });
+      res.status(200).json({ success: true, data: `${JSON.stringify(result)}` });
     } catch (error) {
       user.resetPasswordToken = undefined;
       user.resetPasswordExpire = undefined;
